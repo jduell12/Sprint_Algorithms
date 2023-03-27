1b4fd98e9ff1bed3d16612a27841599dd6177784
Jessica Duell <jessica.duell@pendo.io>
2023-03-27 14:01:04 -0400

    update tests

--- robot_sort/robot_sort.test.js
+++ robot_sort/robot_sort.test.js
@@ -7,75 +7,75 @@ describe('SortingRobot', ()=>{
     const varied = [1, -38, -95, 4, 23, -73, -65, -36, 85, 2, 58, -26, -55, 96, 55, -76, 64, 45, 69, 36, 69, 47, 29, -47, 13, 89, -57, -88, -87, 54, 60, 56, -98, -78, 59, 93, -41, -74, 73, -35, -23, -79, -35, 46, -18, -18, 37, -64, 14, -57, -2, 15, -85, 45, -73, -2, 79, -87, -100, 21, -51, 22, 26, -59, 81, 59, -24, 24, -81, 43, 61, 52, 38, -88, -95, 87, -57, -37, -65, -47, -3, 21, -77, 98, 25, 1, -36, 39, 78, 47, -35, -40, -69, -81, 11, -47, 21, 25, -53, -31]
     
     it('[1] works with small list', ()=>{
-        const robot = SortingRobot(small)
+        const robot = new SortingRobot(small)
         robot.sort();
         expect(robot.list).toEqual([1, 2, 3, 4, 5])
     })
     
     it('[2] works with med list', ()=>{
-        const robot = SortingRobot(med)
+        const robot = new SortingRobot(med)
         robot.sort();
         expect(robot.list).toEqual([
-            0,  1, 10, 11, 12, 13, 14, 15,
-           16, 17, 18, 19,  2, 20, 21, 22,
-           23, 24,  3,  4,  5,  6,  7,  8,
-            9
+            0,  1,  2,  3,  4,  5,  6,  7,
+       8,  9, 10, 11, 12, 13, 14, 15,
+      16, 17, 18, 19, 20, 21, 22, 23,
+      24
          ])
     })
     
     it('[3] works with large list', ()=>{
-        const robot = SortingRobot(large)
+        const robot = new SortingRobot(large)
         robot.sort();
         expect(robot.list).toEqual([
-            0,  1, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
-            2, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29,  3,
-           30, 31, 32, 33, 34, 35, 36, 37, 38, 39,  4, 40,
-           41, 42, 43, 44, 45, 46, 47, 48, 49,  5, 50, 51,
-           52, 53, 54, 55, 56, 57, 58, 59,  6, 60, 61, 62,
-           63, 64, 65, 66, 67, 68, 69,  7, 70, 71, 72, 73,
-           74, 75, 76, 77, 78, 79,  8, 80, 81, 82, 83, 84,
-           85, 86, 87, 88, 89,  9, 90, 91, 92, 93, 94, 95,
-           96, 97, 98, 99
+            0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11,
+            12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23,
+            24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35,
+            36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47,
+            48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59,
+            60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71,
+            72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83,
+            84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95,
+            96, 97, 98, 99
          ])
     })
     
     it('[4] works with large varied list', ()=>{
-        const robot = SortingRobot(varied)
+        const robot = new SortingRobot(varied)
         robot.sort();
         expect(robot.list).toEqual([
-            -100, -18, -18,  -2,  -2, -23, -24, -26,  -3, -31, -35, -35,
-             -35, -36, -36, -37, -38, -40, -41, -47, -47, -47, -51, -53,
-             -55, -57, -57, -57, -59, -64, -65, -65, -69, -73, -73, -74,
-             -76, -77, -78, -79, -81, -81, -85, -87, -87, -88, -88, -95,
-             -95, -98,   1,   1,  11,  13,  14,  15,   2,  21,  21,  21,
-              22,  23,  24,  25,  25,  26,  29,  36,  37,  38,  39,   4,
-              43,  45,  45,  46,  47,  47,  52,  54,  55,  56,  58,  59,
-              59,  60,  61,  64,  69,  69,  73,  78,  79,  81,  85,  87,
-              89,  93,  96,  98
+            -100, -98, -95, -95, -88, -88, -87, -87, -85, -81, -81, -79,
+            -78, -77, -76, -74, -73, -73, -69, -65, -65, -64, -59, -57,
+            -57, -57, -55, -53, -51, -47, -47, -47, -41, -40, -38, -37,
+            -36, -36, -35, -35, -35, -31, -26, -24, -23, -18, -18,  -3,
+             -2,  -2,   1,   1,   2,   4,  11,  13,  14,  15,  21,  21,
+             21,  22,  23,  24,  25,  25,  26,  29,  36,  37,  38,  39,
+             43,  45,  45,  46,  47,  47,  52,  54,  55,  56,  58,  59,
+             59,  60,  61,  64,  69,  69,  73,  78,  79,  81,  85,  87,
+             89,  93,  96,  98
           ])
     })
     
     //stretch tests
     // it('[s-1] small list time is less than 110', ()=>{
-    //     const robot = SortingRobot(small)
+    //     const robot = new SortingRobot(small)
     //     robot.sort();
     //     expect(robot.time).toBeLessThan(110)
     // })
     
     // it('[s-2] med list time is less than 1948', ()=>{
-    //     const robot = SortingRobot(med)
+    //     const robot = new SortingRobot(med)
     //     robot.sort();
     //     expect(robot.time).toBeLessThan(1948)
     // })
     
     // it('[s-3] large list time is less than 27513', ()=>{
-    //     const robot = SortingRobot(large)
+    //     const robot = new SortingRobot(large)
     //     robot.sort();
     //     expect(robot.time).toBeLessThan(27513)
     // })
     
     // it('[s-4] large varied list time is less than 28308', ()=>{
-    //     const robot = SortingRobot(varied)
+    //     const robot = new SortingRobot(varied)
     //     robot.sort();
     //     expect(robot.time).toBeLessThan(28308)
     // })
